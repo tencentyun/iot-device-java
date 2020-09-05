47575a246842f75e6fd2f4921193c0b01f523bad
sevenhhe <sevenhhe@goodow.com>
2020-09-05 10:49:23 +0800

    [设备SDK]支持websocket-MQTT协议[ID859120933] 1、处理无回到事件 2、优化连接逻辑，增加状态机制 3、添加 java 层的调用示例
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192859120933
    
    Change-Id: Ia1ca52eca8cd06d85f6b0727f5a72cb266b5009c

--- hub-device-java/src/main/java/com/tencent/iot/hub/device/java/core/mqtt/TXWebSocketManager.java
+++ hub-device-java/src/main/java/com/tencent/iot/hub/device/java/core/mqtt/TXWebSocketManager.java
@@ -1,13 +1,7 @@
 package com.tencent.iot.hub.device.java.core.mqtt;
 
-import com.tencent.iot.hub.device.java.core.util.Base64;
-import com.tencent.iot.hub.device.java.core.util.HmacSha256;
 
 import org.eclipse.paho.client.mqttv3.MqttException;
-import org.json.JSONObject;
-
-import java.net.URI;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -22,8 +16,6 @@ public class TXWebSocketManager {
     private String PREFIX = "wss://";
 
     private static Map<String, TXWebSocketClient> clients = new ConcurrentHashMap<>();
-
-    public static String path;
 
     private TXWebSocketManager() { }
 
@@ -42,17 +34,13 @@ public class TXWebSocketManager {
 
         String clientId = productId + devicename;
 
-        String uriStr = PREFIX + productId + defaultUriStr;
         if (clients.containsKey(clientId) && clients.get(clientId) != null) {
             // 集合内已经存在连接对象，不需要对连接对象做任何处理
         } else {    // 集合内不存在连接对象，新创建一个连接对象
-            TXWebSocketClient clientRet = null;
             try {
-                System.out.println("serverURI=" + uriStr);
-                System.out.println("clientId=" + clientId);
-                System.out.println("path=" + path);
-                clientRet = new TXWebSocketClient(uriStr, clientId, path);
-                clients.put(clientId, clientRet);
+                String uriStr = PREFIX + productId + defaultUriStr;
+                TXWebSocketClient client = new TXWebSocketClient(uriStr, clientId);
+                clients.put(clientId, client);
             } catch (MqttException e) {
                 e.printStackTrace();
                 System.out.println("e=" + e.toString());
@@ -63,16 +51,19 @@ public class TXWebSocketManager {
     }
 
     public synchronized void releaseClient(String productId, String devicename) {
-        // 禁止移除默认的连接对象
-        if (isEmpty(productId) || isEmpty(devicename)) {
-            return;
-        }
 
         // 移除对象默认关闭连接
         String clientId = productId + devicename;
         TXWebSocketClient clientRet = clients.remove(clientId);
-        if (clientRet.isConnected()) {
-            clientRet.destroy();
+
+        // 不处于断开连接状态的对象需要被销毁
+        if (clientRet.getConnectionState() != ConnectionState.DISCONNECTED &&
+                clientRet.getConnectionState() != ConnectionState.DISCONNECTING) {
+            try {
+                clientRet.disconnect();
+            } catch (MqttException e) {
+                e.printStackTrace();
+            }
         }
     }
 
