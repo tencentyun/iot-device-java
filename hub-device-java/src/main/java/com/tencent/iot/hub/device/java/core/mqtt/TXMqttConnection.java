36af9cf0da4acd01dc99fe0a4514c9c10c14ed10
archurtan <archurspace@gmail.com>
2020-09-21 16:27:23 +0800

    修复浩坤客户问题-ping掉线问题
    等待keepalive周期 * 1.5倍的时间后，会回调OnConnectionLost
    
    Change-Id: I944c9e623303e42e886c1a2be7bad0bd2c4292b0

--- hub-device-java/src/main/java/com/tencent/iot/hub/device/java/core/mqtt/TXMqttConnection.java
+++ hub-device-java/src/main/java/com/tencent/iot/hub/device/java/core/mqtt/TXMqttConnection.java
@@ -246,8 +246,7 @@ public class TXMqttConnection implements MqttCallbackExtended {
 
 		if (mMqttClient == null) {
 			try {
-				mPingSender = new TXAlarmPingSender();
-				mMqttClient = new MqttAsyncClient(mServerURI, mClientId, mMqttPersist, mPingSender);
+				mMqttClient = new MqttAsyncClient(mServerURI, mClientId, mMqttPersist);
 				mMqttClient.setCallback(this);
 				mMqttClient.setBufferOpts(this.bufferOpts);
 				mMqttClient.setManualAcks(false);
