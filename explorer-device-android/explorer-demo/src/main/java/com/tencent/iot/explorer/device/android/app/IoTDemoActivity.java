a07de7196141d249579de484a41334470bddfc98
v_vweisun(孙伟) <532125505@qq.com>
2020-11-26 16:06:58 +0800

    修改Demo入口分为IoT Demo，IoT AI FACE Demo，IoT TRTC Demo
    Change-Id: If80af68833fe051dac2e9f0e914170b3a0072a90

---
+++ explorer-device-android/explorer-demo/src/main/java/com/tencent/iot/explorer/device/android/app/IoTDemoActivity.java
@@ -0,0 +1,45 @@
+package com.tencent.iot.explorer.device.android.app;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class IoTDemoActivity extends AppCompatActivity {
+
+    private Button mIoTDemoBtn;
+    private Button mIoTAiFaceBtn;
+    private Button mIoTTrtcBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iot_demo);
+
+        mIoTDemoBtn = findViewById(R.id.iot_demo);
+        mIoTAiFaceBtn = findViewById(R.id.iot_ai_face);
+        mIoTTrtcBtn = findViewById(R.id.iot_trtc);
+
+        mIoTDemoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(IoTDemoActivity.this, IoTMainActivity.class));
+            }
+        });
+
+        mIoTAiFaceBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mIoTTrtcBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
