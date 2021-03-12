5270f99f2a0fee7ff2a5062cb35e41f9dfd044db
archurtan <archurspace@gmail.com>
2021-03-12 16:29:57 +0800

    增加绑定tid设备的单测
    Change-Id: If5a98b4f903249cd1d24be137d4287f098f6c8fe

---
+++ hub/hub-device-java/src/test/java/TestBindTidDevice.java
@@ -0,0 +1,33 @@
+import com.tencent.iot.hub.device.java.core.dynreg.TXMqttBindDevice;
+import com.tencent.iot.hub.device.java.core.dynreg.TXMqttBindDeviceCallback;
+
+import org.junit.Test;
+
+import unit.test.BuildConfig;
+
+import static org.junit.Assert.*;
+
+public class TestBindTidDevice {
+    private TXMqttBindDevice mqttBindDevice;
+    private String productId = BuildConfig.TESTBINDTIDDEVICE_PRODUCT_ID;
+    private String deviceName = BuildConfig.TESTBINDTIDDEVICE_DEVICE_NAME;
+    private String tid= BuildConfig.TESTBINDTIDDEVICE_TID;
+    private String pemPubKey = BuildConfig.TESTBINDTIDDEVICE_PEM_PUBKEY;
+
+    private TXMqttBindDeviceCallback callback = new TXMqttBindDeviceCallback() {
+        @Override
+        public void onBindFailed(Throwable cause) { }
+
+        @Override
+        public void onBindSuccess(String msg) {
+            System.out.println(msg);
+        }
+    };
+
+    @Test
+    public void testBindTidDevice() {
+//        mqttBindDevice = new TXMqttBindDevice(productId, deviceName, tid, pemPubKey, callback);
+//        mqttBindDevice.doBind();
+        assertTrue(true);
+    }
+}
