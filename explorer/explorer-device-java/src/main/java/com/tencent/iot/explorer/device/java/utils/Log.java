43718643e02bcc41751a27d55e369c92efe831b0
archurtan <archurspace@gmail.com>
2021-05-17 14:26:29 +0800

    重构DataTemplateJson
    Change-Id: I3be0ccd49ad2b533344864c32de1a3d86b970383

---
+++ explorer/explorer-device-java/src/main/java/com/tencent/iot/explorer/device/java/utils/Log.java
@@ -0,0 +1,11 @@
+package com.tencent.iot.explorer.device.java.utils;
+
+public interface Log {
+    void debug(String msg);
+
+    void info(String msg);
+
+    void warn(String msg);
+
+    void error(String msg);
+}
