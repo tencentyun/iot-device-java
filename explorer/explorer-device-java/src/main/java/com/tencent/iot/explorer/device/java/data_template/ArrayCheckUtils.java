0d2ccbe487c9c1cc48aeba90a078a49e6d42195f
whalensun(孙伟) <532125505@qq.com>
2021-05-20 10:17:23 +0800

    完善json校验Array和Struct中元素的类型判断
    Change-Id: Ib434aa12d29c60f147eb9e0bbab734c6873ec306

--- explorer/explorer-device-java/src/main/java/com/tencent/iot/explorer/device/java/data_template/ArrayCheckUtils.java
+++ explorer/explorer-device-java/src/main/java/com/tencent/iot/explorer/device/java/data_template/ArrayCheckUtils.java
@@ -11,30 +11,30 @@ public class ArrayCheckUtils {
         if (value instanceof JSONArray) {
             JSONArray json = (JSONArray) value;
 
-            if (valuesTypeData.getString("type").equals("int")) {
+            if (valuesTypeData.getString("type").equals(TXDataTemplateJson.TYPE_INT)) {
                 for (int i = 0; i < json.length(); i++) {
-                    if (json.getInt(i) < valuesTypeData.getInt("min") ||
+                    if (!(json.get(i) instanceof Integer) || json.getInt(i) < valuesTypeData.getInt("min") ||
                             json.getInt(i) > valuesTypeData.getInt("max")) {
                         return Status.PARAMETER_INVALID;
                     }
                 }
-            } else if (valuesTypeData.getString("type").equals("string")) {
+            } else if (valuesTypeData.getString("type").equals(TXDataTemplateJson.TYPE_STRING)) {
                 for (int i = 0; i < json.length(); i++) {
-                    if (json.getString(i).length() < valuesTypeData.getInt("min") ||
+                    if (!(json.get(i) instanceof String) || json.getString(i).length() < valuesTypeData.getInt("min") ||
                             json.getString(i).length() > valuesTypeData.getInt("max")) {
                         return Status.PARAMETER_INVALID;
                     }
                 }
-            } else if (valuesTypeData.getString("type").equals("float")) {
+            } else if (valuesTypeData.getString("type").equals(TXDataTemplateJson.TYPE_FLOAT)) {
                 for (int i = 0; i < json.length(); i++) {
                     float currentNum = Float.valueOf(json.get(i).toString());
                     float min = Float.valueOf(valuesTypeData.getString("min"));
                     float max = Float.valueOf(valuesTypeData.getString("max"));
-                    if (currentNum < min || currentNum > max) {
+                    if (!(json.get(i) instanceof Double) || currentNum < min || currentNum > max) {
                         return Status.PARAMETER_INVALID;
                     }
                 }
-            } else if (valuesTypeData.getString("type").equals("struct")) {
+            } else if (valuesTypeData.getString("type").equals(TXDataTemplateJson.TYPE_STRUCT)) {
                 for (int i = 0; i < json.length(); i++) {
                     JSONObject ele = json.getJSONObject(i);
                     JSONArray valuesType = valuesTypeData.getJSONArray("specs");
