755f877b50c05775a1cf92e3cf338a72ef7a20ac
archurtan <archurtan@tencent.com>
2021-06-02 15:43:46 +0800

    hub模块去重
    Change-Id: Ia4ce4e97fe4292d6af7d531d004f08a49d01d34c

--- hub/hub-device-android/src/main/java/com/tencent/iot/hub/device/android/service/TXMqttConnectOptions.java
+++ hub/hub-device-android/src/main/java/com/tencent/iot/hub/device/android/service/TXMqttConnectOptions.java
@@ -9,37 +9,7 @@ import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
  * 该类负责序列化MqttConnectOptions相关信息
  */
 
-public class TXMqttConnectOptions implements Parcelable {
-
-    private int mConnectionTimeout = MqttConnectOptions.CONNECTION_TIMEOUT_DEFAULT;
-    private int mKeepAliveInterval = MqttConnectOptions.KEEP_ALIVE_INTERVAL_DEFAULT;
-    private boolean mAutomaticReconnect = false;
-    private boolean mCleanSession = MqttConnectOptions.CLEAN_SESSION_DEFAULT;
-
-    /**
-     * 是否使用shadow
-     */
-    private boolean mUseShadow = false;
-
-    /**
-     * 是否采用非对称加密
-     */
-    private boolean mAsymcEncryption = true;
-
-    /**
-     * 设备证书名
-     */
-    private String mDeviceCertName;
-
-    /**
-     * 设备私钥文件名
-     */
-    private String mDeviceKeyName;
-
-    /**
-     * 设备密码
-     */
-    private String mSecretKey;
+public class TXMqttConnectOptions extends com.tencent.iot.hub.device.java.service.TXMqttConnectOptions implements Parcelable {
 
     public static final Creator<TXMqttConnectOptions> CREATOR = new Creator<TXMqttConnectOptions>() {
         @Override
@@ -56,32 +26,19 @@ public class TXMqttConnectOptions implements Parcelable {
     public TXMqttConnectOptions() {
     }
 
-    public TXMqttConnectOptions(int mConnectionTimeout, int mKeepAliveInterval, boolean mAutomaticReconnect, boolean mCleanSession,
-                                boolean mAsymcEncryption, String mDeviceCertName, String mDeviceKeyName, String mSecretKey) {
-        this.mConnectionTimeout = mConnectionTimeout;
-        this.mKeepAliveInterval = mKeepAliveInterval;
-        this.mAutomaticReconnect = mAutomaticReconnect;
-        this.mCleanSession = mCleanSession;
-        this.mAsymcEncryption = mAsymcEncryption;
-        this.mDeviceCertName = mDeviceCertName;
-        this.mDeviceKeyName = mDeviceKeyName;
-        this.mSecretKey = mSecretKey;
-    }
-
     protected TXMqttConnectOptions(Parcel in) {
-        mConnectionTimeout = in.readInt();
-        mKeepAliveInterval = in.readInt();
-        mDeviceCertName = in.readString();
-        mDeviceKeyName = in.readString();
-        mSecretKey = in.readString();
+        setConnectionTimeout(in.readInt());
+        setKeepAliveInterval(in.readInt());
+        setDeviceCertName(in.readString());
+        setDeviceKeyName(in.readString());
+        setSecretKey(in.readString());
         boolean[] booleanArray = new boolean[4];
         in.readBooleanArray(booleanArray);
-        mAutomaticReconnect = booleanArray[0];
-        mUseShadow = booleanArray[1];
-        mCleanSession = booleanArray[2];
-        mAsymcEncryption = booleanArray[3];
+        setAutomaticReconnect(booleanArray[0]);
+        setUseShadow(booleanArray[1]);
+        setCleanSession(booleanArray[2]);
+        setAsymcEncryption(booleanArray[3]);
     }
-
 
     @Override
     public int describeContents() {
@@ -90,93 +47,12 @@ public class TXMqttConnectOptions implements Parcelable {
 
     @Override
     public void writeToParcel(Parcel out, int flag) {
-        out.writeInt(mConnectionTimeout);
-        out.writeInt(mKeepAliveInterval);
-        out.writeString(mDeviceCertName);
-        out.writeString(mDeviceKeyName);
-        out.writeString(mSecretKey);
-        boolean[] booleanArray = new boolean[]{mAutomaticReconnect, mUseShadow, mCleanSession, mAsymcEncryption};
+        out.writeInt(getConnectionTimeout());
+        out.writeInt(getKeepAliveInterval());
+        out.writeString(getDeviceCertName());
+        out.writeString(getDeviceKeyName());
+        out.writeString(getSecretKey());
+        boolean[] booleanArray = new boolean[]{isAutomaticReconnect(), isUseShadow(), isCleanSession(), isAsymcEncryption()};
         out.writeBooleanArray(booleanArray);
-    }
-
-    public int getConnectionTimeout() {
-        return mConnectionTimeout;
-    }
-
-    public TXMqttConnectOptions setConnectionTimeout(int mConnectionTimeout) {
-        this.mConnectionTimeout = mConnectionTimeout;
-        return this;
-    }
-
-    public int getKeepAliveInterval() {
-        return mKeepAliveInterval;
-    }
-
-    public TXMqttConnectOptions setKeepAliveInterval(int mKeepAliveInterval) {
-        this.mKeepAliveInterval = mKeepAliveInterval;
-        return this;
-    }
-
-    public boolean isAutomaticReconnect() {
-        return mAutomaticReconnect;
-    }
-
-    public TXMqttConnectOptions setAutomaticReconnect(boolean mAutomaticReconnect) {
-        this.mAutomaticReconnect = mAutomaticReconnect;
-        return this;
-    }
-
-    public boolean isCleanSession() {
-        return mCleanSession;
-    }
-
-    public TXMqttConnectOptions setCleanSession(boolean mCleanSession) {
-        this.mCleanSession = mCleanSession;
-        return this;
-    }
-
-    public boolean isAsymcEncryption() {
-        return mAsymcEncryption;
-    }
-
-    public TXMqttConnectOptions setAsymcEncryption(boolean mAsymcEncryption) {
-        this.mAsymcEncryption = mAsymcEncryption;
-        return this;
-    }
-
-    public String getDeviceCertName() {
-        return mDeviceCertName;
-    }
-
-    public TXMqttConnectOptions setDeviceCertName(String mDeviceCertName) {
-        this.mDeviceCertName = mDeviceCertName;
-        return this;
-    }
-
-    public String getDeviceKeyName() {
-        return mDeviceKeyName;
-    }
-
-    public TXMqttConnectOptions setDeviceKeyName(String mDeviceKeyName) {
-        this.mDeviceKeyName = mDeviceKeyName;
-        return this;
-    }
-
-    public String getSecretKey() {
-        return mSecretKey;
-    }
-
-    public TXMqttConnectOptions setSecretKey(String secretKey) {
-        this.mSecretKey = secretKey;
-        return this;
-    }
-
-    protected boolean isUseShadow() {
-        return mUseShadow;
-    }
-
-    protected TXMqttConnectOptions setUseShadow(boolean mUseShadow) {
-        this.mUseShadow = mUseShadow;
-        return this;
     }
 }
