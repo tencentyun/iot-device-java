39f87692734db4d4d6cb35e2ed804dd933a7fab0
whalensun(孙伟) <532125505@qq.com>
2021-06-09 19:44:53 +0800

    修复上报_sys_extra_info的bug。
    Change-Id: I565267e60e38a8b101462b71ef81d91933ad4fdb

--- explorer/explorer-device-rtc/src/main/java/com/tencent/iot/explorer/device/rtc/data_template/TXTRTCDataTemplate.java
+++ explorer/explorer-device-rtc/src/main/java/com/tencent/iot/explorer/device/rtc/data_template/TXTRTCDataTemplate.java
@@ -79,15 +79,12 @@ public class TXTRTCDataTemplate extends TXDataTemplate {
                         if (params.has(TXTRTCDataTemplateConstants.PROPERTY_SYS_AGENT)) {
                             userAgent = params.getString(TXTRTCDataTemplateConstants.PROPERTY_SYS_AGENT);
                         }
-                        if (mIsBusy && !mCurrentCallingUserid.equals(userid)) { //非当前设备的通话用户的请求忽略
+                        if (mIsBusy && !mCurrentCallingUserid.equals(userid) && callStatus == TRTCCallStatus.TYPE_CALLING) { //非当前设备的通话用户的请求忽略
+                            reportExtraInfoRejectUserId(userid);
                             return;
                         }
                         if (!mIsBusy || callStatus != TRTCCallStatus.TYPE_CALLING) {
                             mTrtcCallBack.onGetCallStatusCallBack(callStatus, userid, userAgent, TRTCCalling.TYPE_VIDEO_CALL);
-                        }
-                        if (mIsBusy && callStatus == TRTCCallStatus.TYPE_CALLING) { //接收到其他用户呼叫请求
-                            reportExtraInfoRejectUserId(userid);
-                            return;
                         }
                         if (callStatus == TRTCCallStatus.TYPE_IDLE_OR_REFUSE) {
                             mIsBusy = false;
@@ -106,15 +103,12 @@ public class TXTRTCDataTemplate extends TXDataTemplate {
                         if (params.has(TXTRTCDataTemplateConstants.PROPERTY_SYS_AGENT)) {
                             userAgent = params.getString(TXTRTCDataTemplateConstants.PROPERTY_SYS_AGENT);
                         }
-                        if (mIsBusy && !mCurrentCallingUserid.equals(userid)) { //非当前设备的通话用户的请求忽略
+                        if (mIsBusy && !mCurrentCallingUserid.equals(userid) && callStatus == TRTCCallStatus.TYPE_CALLING) { //非当前设备的通话用户的请求忽略
+                            reportExtraInfoRejectUserId(userid);
                             return;
                         }
                         if (!mIsBusy || callStatus != TRTCCallStatus.TYPE_CALLING) {
                             mTrtcCallBack.onGetCallStatusCallBack(callStatus, userid, userAgent, TRTCCalling.TYPE_AUDIO_CALL);
-                        }
-                        if (mIsBusy && callStatus == TRTCCallStatus.TYPE_CALLING) { //接收到其他用户呼叫请求
-                            reportExtraInfoRejectUserId(userid);
-                            return;
                         }
                         if (callStatus == TRTCCallStatus.TYPE_IDLE_OR_REFUSE) {
                             mIsBusy = false;
