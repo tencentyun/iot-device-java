b6bab81a88458b3f924dafcc761dcd112b3c3d56
archurtan <archurtan@tencent.com>
2021-06-18 16:04:13 +0800

    透传java sdk slf4日志 到 android层
    Change-Id: If0c81d82e318532e7d9bf6e5e4610113698eb06a

---
+++ hub/hub-device-java/src/main/java/com/tencent/iot/hub/device/java/utils/Loggor.java
@@ -0,0 +1,45 @@
+package com.tencent.iot.hub.device.java.utils;
+
+import org.slf4j.Logger;
+
+public class Loggor {
+
+    private static Logger logger;
+    private static LogCallBack logCallback;
+
+    public static void setLogger(Logger _logger) {
+        logger = _logger;
+    }
+
+    public static void setLogCallback(LogCallBack callback) {
+        logCallback = callback;
+    }
+
+    public static void error(String tag, String msg) {
+        logger.error(msg);
+        if (logCallback != null) {
+            logCallback.error(tag, msg);
+        }
+    }
+
+    public static void warn(String tag, String msg) {
+        logger.warn(msg);
+        if (logCallback != null) {
+            logCallback.warn(tag, msg);
+        }
+    }
+
+    public static void info(String tag, String msg) {
+        logger.info(msg);
+        if (logCallback != null) {
+            logCallback.info(tag, msg);
+        }
+    }
+
+    public static void debug(String tag, String msg) {
+        logger.debug(msg);
+        if (logCallback != null) {
+            logCallback.debug(tag, msg);
+        }
+    }
+}
