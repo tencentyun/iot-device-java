ce25fa90b48bf0b6db8071718d2c02419fd1b7a7
archurtan <archurtan@tencent.com>
2021-06-22 16:45:32 +0800

    调整CI（从iot-thirdparty-java仓拉取tme）
    Change-Id: Ie707713533af23da5df0ac718c01774ced1324be

--- .github/scripts/updateValuesForTme.sh
+++ .github/scripts/updateValuesForTme.sh
@@ -4,11 +4,11 @@
 mkdir explorer/explorer-device-tme/libs/
 
 # 拉取 tme sdk
-git clone https://$GIT_ACCESS_TOKEN@github.com/tencentyun/iot-thirdparty-android.git
+git clone https://$GIT_ACCESS_TOKEN@github.com/tencentyun/iot-thirdparty-java.git
 
 # 解密
-gpg -d --passphrase "$DECRYPT_PASSPHRASE" --batch --quiet iot-thirdparty-android/tme/ultimatelib-v1.0-200024-release.aar.asc > explorer/explorer-device-tme/libs/ultimatelib-v1.0-200024-release.aar
-gpg -d --passphrase "$DECRYPT_PASSPHRASE" --batch --quiet iot-thirdparty-android/tme/ultimatetv-v0.9.1-arm-release.aar.asc > explorer/explorer-device-tme/libs/ultimatetv-v0.9.1-arm-release.aar
+gpg -d --passphrase "$DECRYPT_PASSPHRASE" --batch --quiet iot-thirdparty-java/tme/ultimatelib-v1.0-200024-release.aar.asc > explorer/explorer-device-tme/libs/ultimatelib-v1.0-200024-release.aar
+gpg -d --passphrase "$DECRYPT_PASSPHRASE" --batch --quiet iot-thirdparty-java/tme/ultimatetv-v0.9.1-arm-release.aar.asc > explorer/explorer-device-tme/libs/ultimatetv-v0.9.1-arm-release.aar
 
 # 启用tme
 sed -i 's#//-##g' settings.gradle
