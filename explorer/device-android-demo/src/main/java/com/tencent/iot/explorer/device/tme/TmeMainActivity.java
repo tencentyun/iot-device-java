63958ba624e07453ed61c54ed2f4067893ccaa1c
archurtan <archurtan@tencent.com>
2021-06-22 16:40:14 +0800

    增加设备上下线逻辑
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192865335171
    
    Change-Id: If62eed193c4d05b563ab682726efa78aa1b2e195

--- explorer/device-android-demo/src/main/java/com/tencent/iot/explorer/device/tme/TmeMainActivity.java
+++ explorer/device-android-demo/src/main/java/com/tencent/iot/explorer/device/tme/TmeMainActivity.java
@@ -3,14 +3,39 @@ package com.tencent.iot.explorer.device.tme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.tencent.iot.explorer.device.android.app.R;
 
-public class TmeMainActivity extends AppCompatActivity {
+public class TmeMainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mOnlineBtn;
+    private Button mOfflineBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tme_main);
+        initView();
+    }
+
+    private void initView() {
+        mOnlineBtn = findViewById(R.id.online);
+        mOfflineBtn = findViewById(R.id.offline);
+        mOnlineBtn.setOnClickListener(this);
+        mOfflineBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.online:
+                break;
+            case R.id.offline:
+                break;
+            default:
+                break;
+        }
     }
 }
