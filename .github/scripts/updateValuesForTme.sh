e3d1b429706919724e159831de104a772ea7e784
archurtan <archurtan@tencent.com>
2021-06-22 11:30:40 +0800

    调整CI
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192865335171
    
    Change-Id: I9f5bca56935c670394af9d3cb48daa88927d1f76

---
+++ .github/scripts/updateValuesForTme.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# 创建libs目录
+mkdir explorer/explorer-device-tme/libs/
+
+# 拉取 tme sdk
+git clone https://$GIT_ACCESS_TOKEN@github.com/tencentyun/iot-thirdparty-android.git
+
+# 解密
+gpg -d --passphrase "$DECRYPT_PASSPHRASE" --batch --quiet iot-thirdparty-android/tme/ultimatelib-v1.0-200024-release.aar.asc > explorer/explorer-device-tme/libs/ultimatelib-v1.0-200024-release.aar
+gpg -d --passphrase "$DECRYPT_PASSPHRASE" --batch --quiet iot-thirdparty-android/tme/ultimatetv-v0.9.1-arm-release.aar.asc > explorer/explorer-device-tme/libs/ultimatetv-v0.9.1-arm-release.aar
+
+# 启用tme
+sed -i 's#//-##g' settings.gradle
+
+# sdkdemo 启用tme
+sed -i 's#com/tencent/iot/explorer/device/tme/##g' explorer/device-android-demo/build.gradle
+
