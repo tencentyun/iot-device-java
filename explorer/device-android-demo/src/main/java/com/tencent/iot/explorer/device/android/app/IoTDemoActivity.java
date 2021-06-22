98714736d84839e2772a26426653ae37dbb7e7c8
archurtan <archurtan@tencent.com>
2021-06-22 09:29:06 +0800

    新增TME module
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192865335171
    
    Change-Id: I919741ae2134e9e851d660ce0369cef5578e7e27

--- explorer/device-android-demo/src/main/java/com/tencent/iot/explorer/device/android/app/IoTDemoActivity.java
+++ explorer/device-android-demo/src/main/java/com/tencent/iot/explorer/device/android/app/IoTDemoActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.tencent.iot.explorer.device.face.AuthActivity;
 import com.tencent.iot.explorer.device.rtc.TRTCMainActivity;
@@ -14,6 +15,7 @@ public class IoTDemoActivity extends AppCompatActivity {
     private Button mIoTDemoBtn;
     private Button mIoTAiFaceBtn;
     private Button mIoTTrtcBtn;
+    private Button mIoTTmeBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,7 @@ public class IoTDemoActivity extends AppCompatActivity {
         mIoTDemoBtn = findViewById(R.id.iot_demo);
         mIoTAiFaceBtn = findViewById(R.id.iot_ai_face);
         mIoTTrtcBtn = findViewById(R.id.iot_trtc);
+        mIoTTmeBtn = findViewById(R.id.iot_tme);
 
         mIoTDemoBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,6 +45,19 @@ public class IoTDemoActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(IoTDemoActivity.this, TRTCMainActivity.class));
+            }
+        });
+
+        mIoTTmeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Class c = Class.forName("com.tencent.iot.explorer.device.tme.TmeMainActivity");
+                    startActivity(new Intent(IoTDemoActivity.this, c));
+                } catch (ClassNotFoundException e) {
+                    Toast.makeText(IoTDemoActivity.this, "请线下联系", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
             }
         });
     }
