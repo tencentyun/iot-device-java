d00b9b3b0ba3cbd43220b57531257ccd3960f6a2
archurtan <archurtan@tencent.com>
2021-06-22 09:29:06 +0800

    新增TME module
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192865335171
    
    Change-Id: I919741ae2134e9e851d660ce0369cef5578e7e27

---
+++ explorer/device-android-demo/src/main/java/com/tencent/iot/explorer/device/tme/TmeMainActivity.java
@@ -0,0 +1,17 @@
+package com.tencent.iot.explorer.device.tme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tencent.iot.explorer.device.android.app.R;
+
+public class TmeMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tme_main);
+        xxx
+    }
+}
