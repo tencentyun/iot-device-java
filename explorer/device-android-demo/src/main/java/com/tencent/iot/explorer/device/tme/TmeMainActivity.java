093079ec75a30aa7c47df94fb64764dbc69cdd36
archurtan <archurtan@tencent.com>
2021-06-22 16:40:14 +0800

    增加设备上下线逻辑
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192865335171
    
    Change-Id: If62eed193c4d05b563ab682726efa78aa1b2e195

--- explorer/device-android-demo/src/main/java/com/tencent/iot/explorer/device/tme/TmeMainActivity.java
+++ explorer/device-android-demo/src/main/java/com/tencent/iot/explorer/device/tme/TmeMainActivity.java
@@ -3,14 +3,188 @@ package com.tencent.iot.explorer.device.tme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.tencent.iot.explorer.device.android.app.R;
+import com.tencent.iot.explorer.device.android.utils.TXLog;
+import com.tencent.iot.explorer.device.java.data_template.TXDataTemplateDownStreamCallBack;
+import com.tencent.iot.explorer.device.java.mqtt.TXMqttRequest;
+import com.tencent.iot.explorer.device.tme.data_template.TmeDataTemplateSample;
+import com.tencent.iot.hub.device.java.core.common.Status;
+import com.tencent.iot.hub.device.java.core.mqtt.TXMqttActionCallBack;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.json.JSONObject;
+import java.util.Arrays;
 
-public class TmeMainActivity extends AppCompatActivity {
+
+public class TmeMainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = TmeMainActivity.class.getSimpleName();
+
+    //传入null，即使用腾讯云物联网通信默认地址 "${ProductId}.iotcloud.tencentdevices.com:8883"  https://cloud.tencent.com/document/product/634/32546
+    private String mBrokerURL = null;
+    private String mProductID = "";
+    private String mDevName = "";
+    private String mDevPSK = ""; //若使用证书验证，设为null
+
+    private String mDevCert = ""; // Cert String
+    private String mDevPriv = ""; // Priv String
+
+    private Button mOnlineBtn;
+    private Button mOfflineBtn;
+
+    private final static String JSON_FILE_NAME = "tme_speaker.json";
+
+    private TmeDataTemplateSample mDataTemplateSample;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tme_main);
+        initView();
+    }
+
+    private void initView() {
+        mOnlineBtn = findViewById(R.id.online);
+        mOfflineBtn = findViewById(R.id.offline);
+        mOnlineBtn.setOnClickListener(this);
+        mOfflineBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.online: {
+                if (mDataTemplateSample != null) return;
+                mDataTemplateSample = new TmeDataTemplateSample(TmeMainActivity.this,
+                        mBrokerURL, mProductID, mDevName, mDevPSK,
+                        new SelfMqttActionCallBack(), JSON_FILE_NAME, new SelfDownStreamCallBack());
+                mDataTemplateSample.connect();
+            }
+            break;
+            case R.id.offline: {
+                if (mDataTemplateSample == null) return;
+                mDataTemplateSample.disconnect();
+                mDataTemplateSample = null;
+            }
+            break;
+            default:
+                break;
+        }
+    }
+
+
+    private class SelfMqttActionCallBack extends TXMqttActionCallBack {
+
+        @Override
+        public void onConnectCompleted(Status status, boolean reconnect, Object userContext, String msg) {
+            String userContextInfo = "";
+            if (userContext instanceof TXMqttRequest) {
+                userContextInfo = userContext.toString();
+            }
+            String logInfo = String.format("onConnectCompleted, status[%s], reconnect[%b], userContext[%s], msg[%s]",
+                    status.name(), reconnect, userContextInfo, msg);
+            if (mDataTemplateSample != null) {
+                if (!reconnect) {
+                    mDataTemplateSample.subscribeTopic();
+                }
+            }
+        }
+
+        @Override
+        public void onConnectionLost(Throwable cause) {
+            String logInfo = String.format("onConnectionLost, cause[%s]", cause.toString());
+            TXLog.e(TAG, logInfo);
+        }
+
+        @Override
+        public void onDisconnectCompleted(Status status, Object userContext, String msg) {
+            String userContextInfo = "";
+            if (userContext instanceof TXMqttRequest) {
+                userContextInfo = userContext.toString();
+            }
+            String logInfo = String.format("onDisconnectCompleted, status[%s], userContext[%s], msg[%s]", status.name(), userContextInfo, msg);
+            TXLog.d(TAG, logInfo);
+        }
+
+        @Override
+        public void onPublishCompleted(Status status, IMqttToken token, Object userContext, String errMsg) {
+            String userContextInfo = "";
+            if (userContext instanceof TXMqttRequest) {
+                userContextInfo = userContext.toString();
+            }
+            String logInfo = String.format("onPublishCompleted, status[%s], topics[%s],  userContext[%s], errMsg[%s]",
+                    status.name(), Arrays.toString(token.getTopics()), userContextInfo, errMsg);
+            TXLog.d(TAG, logInfo);
+        }
+
+        @Override
+        public void onSubscribeCompleted(Status status, IMqttToken asyncActionToken, Object userContext, String errMsg) {
+            String userContextInfo = "";
+            if (userContext instanceof TXMqttRequest) {
+                userContextInfo = userContext.toString();
+            }
+            String logInfo = String.format("onSubscribeCompleted, status[%s], topics[%s], userContext[%s], errMsg[%s]",
+                    status.name(), Arrays.toString(asyncActionToken.getTopics()), userContextInfo, errMsg);
+            if (Status.ERROR == status) {
+                TXLog.e(TAG, logInfo);
+            } else {
+                TXLog.d(TAG, logInfo);
+            }
+            if (Status.OK != mDataTemplateSample.propertyGetStatus("report", false)) {
+                TXLog.e(TAG, "property get status failed!");
+            }
+        }
+
+        @Override
+        public void onUnSubscribeCompleted(Status status, IMqttToken asyncActionToken, Object userContext, String errMsg) {
+            String userContextInfo = "";
+            if (userContext instanceof TXMqttRequest) {
+                userContextInfo = userContext.toString();
+            }
+            String logInfo = String.format("onUnSubscribeCompleted, status[%s], topics[%s], userContext[%s], errMsg[%s]",
+                    status.name(), Arrays.toString(asyncActionToken.getTopics()), userContextInfo, errMsg);
+            TXLog.d(TAG, logInfo);
+        }
+
+        @Override
+        public void onMessageReceived(final String topic, final MqttMessage message) {
+            String logInfo = String.format("receive command, topic[%s], message[%s]", topic, message.toString());
+            TXLog.d(TAG, logInfo);
+        }
+    }
+
+    private static class SelfDownStreamCallBack extends TXDataTemplateDownStreamCallBack {
+        @Override
+        public void onReplyCallBack(String replyMsg) {
+            //可根据自己需求进行处理属性上报以及事件的回复，根据需求填写
+            TXLog.d(TAG, "reply received : " + replyMsg);
+        }
+
+        @Override
+        public void onGetStatusReplyCallBack(JSONObject data) {
+
+        }
+
+        @Override
+        public JSONObject onControlCallBack(JSONObject msg) {
+            TXLog.d(TAG, "control down stream message received : " + msg);
+            return null;
+        }
+
+        @Override
+        public JSONObject onActionCallBack(String actionId, JSONObject params) {
+            TXLog.d(TAG, String.format("onActionCallBack : actionId=[%s], params=[%s]", actionId, params.toString()));
+            return null;
+        }
+
+        @Override
+        public void onUnbindDeviceCallBack(String msg) {
+            Log.d(TAG, "unbind device received : " + msg);
+        }
     }
 }
