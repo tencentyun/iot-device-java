cb33b228aaf758811ec75cfed9a13c6de5ac790b
archurtan <archurtan@tencent.com>
2021-06-29 19:41:58 +0800

    添加播放进度UI/音量UI以及对应的控制逻辑
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192865335171
    
    Change-Id: Ib926ffd47fd186d9edc9b927ab8adf2ed202376d

--- explorer/explorer-device-tme/src/main/java/com/tencent/iot/explorer/device/tme/consts/Common.java
+++ explorer/explorer-device-tme/src/main/java/com/tencent/iot/explorer/device/tme/consts/Common.java
@@ -2,15 +2,37 @@ package com.tencent.iot.explorer.device.tme.consts;
 
 public class Common {
 
-    public static final String PRE_NEXT = "pre_next";
+    public static final String PROPERTY_PRE_NEXT = "pre_next";
 
-    public static final String PAUSE_PLAY = "pause_play";
+    public static final String PROPERTY_PAUSE_PLAY = "pause_play";
 
-    public static final String PLAY_MODE = "play_mode";
+    public static final String PROPERTY_PLAY_MODE = "play_mode";
 
-    public static final String SONG_ID = "song_id";
+    public static final String PROPERTY_SONG_ID = "song_id";
 
-    public static final String CUR_PLAY_LIST = "cur_play_list";
+    public static final String PROPERTY_CUR_PLAY_LIST = "cur_play_list";
 
-    public static final String SET_SONG_LIST = "set_song_list";
+    public static final String PROPERTY_SET_SONG_LIST = "set_song_list";
+
+    public static final String PROPERTY_VOLUME = "volume";
+
+    public static final String PROPERTY_PLAY_POSITION = "play_position";
+
+    public static final String PROPERTY_CUR_SONG_ID = "cur_song_id";
+
+    public static final String PROPERTY_CONTROL_SEQ = "control_seq";
+
+    public static final String PROPERTY_RECOMMEND_QUALITY = "recommend_quality";
+
+    public static final String PLAY_TYPE = "play_type";
+
+    public static final String PLAY_ID = "play_id";
+
+    public static final String PLAY_TYPE_ALBUM = "album";
+
+    public static final String PLAY_TYPE_SONGLIST = "songlist";
+
+    public static final int TOTOAL_DURATION = 7200;
+
+    public static final int TOTOAL_VOLUME_DURATION = 100;
 }
