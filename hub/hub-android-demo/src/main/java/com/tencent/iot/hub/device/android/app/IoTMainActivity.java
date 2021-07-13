5c157114b92f1802d4dd5c262d6ee370fd55251e
heyawei <379135467@qq.com>
2021-07-13 17:47:30 +0800

    【公有云Hub】上线国际站（中文字符使用字符资源依赖）
    http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192866322527
    
    Change-Id: If663bfdd8a6211b955c355cd1c3da1e5a44412e7

--- hub/hub-android-demo/src/main/java/com/tencent/iot/hub/device/android/app/IoTMainActivity.java
+++ hub/hub-android-demo/src/main/java/com/tencent/iot/hub/device/android/app/IoTMainActivity.java
@@ -80,7 +80,11 @@ public class IoTMainActivity extends AppCompatActivity implements View.OnClickLi
         // 下面配置是为了让sdk中用log4j记录的日志可以输出至logcat
         LogConfigurator logConfigurator = new LogConfigurator();
         logConfigurator.setFileName(Environment.getExternalStorageDirectory() + File.separator + "hub-demo.log");
-        logConfigurator.configure();
+        try {
+            logConfigurator.configure();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -90,7 +94,7 @@ public class IoTMainActivity extends AppCompatActivity implements View.OnClickLi
         if (requestCode == REQUEST_EXTERNAL_STORAGE) {
             for (int i = 0; i < permissions.length; i++) {
                 if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
-                    Log.e(TAG, "必要权限申请失败");
+                    Log.e(TAG, getString(R.string.request_permission_first));
                     finish();
                 } else {
                     initLogConfigurator();
