7a55cd9daf3f176c619c0e9ea0049b6e84125a9a
archurtan <archurtan@tencent.com>
2021-11-04 14:36:14 +0800

    使用concurrent改造hub单元测试
    Change-Id: I460c6f3356705c75164a07e55f7540847107aa6e

--- hub/hub-device-java/src/test/java/com/tencent/iot/hub/device/java/core/dynreg/DynregDevSampleTest.java
+++ hub/hub-device-java/src/test/java/com/tencent/iot/hub/device/java/core/dynreg/DynregDevSampleTest.java
@@ -5,8 +5,6 @@ import com.tencent.iot.hub.device.java.core.log.TXMqttLogCallBack;
 import com.tencent.iot.hub.device.java.core.mqtt.TXMqttActionCallBack;
 import com.tencent.iot.hub.device.java.core.mqtt.TXMqttConnection;
 import com.tencent.iot.hub.device.java.core.mqtt.TXMqttConstants;
-import com.tencent.iot.hub.device.java.core.mqtt.TXOTACallBack;
-import com.tencent.iot.hub.device.java.core.mqtt.TXOTAConstansts;
 import com.tencent.iot.hub.device.java.core.util.AsymcSslUtils;
 import com.tencent.iot.hub.device.java.utils.Loggor;
 
@@ -27,6 +25,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.util.Arrays;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import hub.unit.test.BuildConfig;
@@ -39,13 +39,10 @@ import static org.junit.Assert.assertSame;
 public class DynregDevSampleTest {
 
     private static final Logger LOG = LoggerFactory.getLogger(DynregDevSampleTest.class);
-
     private static final String TAG = DynregDevSampleTest.class.getSimpleName();
-
-    private static String path2Store = System.getProperty("user.dir");
+    private static final String mLogPath = System.getProperty("user.dir") + "/hub/hub-device-java/src/test/resources/";
 
     private static String mBrokerURL = null;  //传入null，即使用腾讯云物联网通信默认地址 "${ProductId}.iotcloud.tencentdevices.com:8883"  https://cloud.tencent.com/document/product/634/32546
-
     private static String mProductID = BuildConfig.TESTDYNREGDEVSAMPLE_PRODUCT_ID;
     private static String mDevName = BuildConfig.TESTDYNREGDEVSAMPLE_DEVICE_NAME;
     private static String mDevPSK = "DEVICE_PSK";
@@ -56,49 +53,35 @@ public class DynregDevSampleTest {
     private static TXMqttConnection mqttconnection;
     private static MqttConnectOptions options;
 
-    private static final String mLogPath = System.getProperty("user.dir") + "/hub/hub-device-java/src/test/resources/";
-
-    private static final AtomicInteger requestID = new AtomicInteger(0);
 
     static {
         Loggor.setLogger(LOG);
     }
 
     private static void connect() {
-        try {
-            Thread.sleep(2000);
-            // init connection
-            options = new MqttConnectOptions();
-            options.setConnectionTimeout(8);
-            options.setKeepAliveInterval(60);
-            options.setAutomaticReconnect(true);
-            //客户端证书文件名  mDevPSK是设备秘钥
-            if (mDevPSK != null && mDevPSK.length() != 0) {
-                Loggor.info(TAG, "Using PSK");
-                // options.setSocketFactory(AsymcSslUtils.getSocketFactory());   如果您使用的是3.3.0及以下版本的 hub-device-java sdk，由于密钥认证默认配置的ssl://的url，请添加此句setSocketFactory配置。
-            } else {
-                Loggor.info(TAG, "Using cert stream " + mDevPriv + "  " + mDevCert);
-                options.setSocketFactory(AsymcSslUtils.getSocketFactoryByStream(new ByteArrayInputStream(mDevCert.getBytes()), new ByteArrayInputStream(mDevPriv.getBytes())));
-            }
-            mqttconnection = new TXMqttConnection(mBrokerURL, mProductID, mDevName, mDevPSK, null, null, true, new SelfMqttLogCallBack(), new callBack());
-            mqttconnection.connect(options, null);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        // init connection
+        options = new MqttConnectOptions();
+        options.setConnectionTimeout(8);
+        options.setKeepAliveInterval(60);
+        options.setAutomaticReconnect(true);
+        if (mDevPSK != null && mDevPSK.length() != 0) {
+            Loggor.info(TAG, "Using PSK");
+            // options.setSocketFactory(AsymcSslUtils.getSocketFactory());   如果您使用的是3.3.0及以下版本的 hub-device-java sdk，由于密钥认证默认配置的ssl://的url，请添加此句setSocketFactory配置。
+        } else {
+            Loggor.info(TAG, "Using cert stream " + mDevPriv + "  " + mDevCert);
+            options.setSocketFactory(AsymcSslUtils.getSocketFactoryByStream(new ByteArrayInputStream(mDevCert.getBytes()), new ByteArrayInputStream(mDevPriv.getBytes())));
         }
+        mqttconnection = new TXMqttConnection(mBrokerURL, mProductID, mDevName, mDevPSK, null, null, true, new SelfMqttLogCallBack(), new callBack());
+        mqttconnection.connect(options, null);
     }
 
     private static void dynReg() {
-        try {
-            Thread.sleep(2000);
-            Loggor.debug(TAG, "Test Dynamic");
-            TXMqttDynreg dynreg = new TXMqttDynreg(mProductID, mProductSecret, mDevName, new SelfMqttDynregCallback());//初始化TXMqttDynreg
-            if (dynreg.doDynamicRegister()) {//调起动态注册
-                Loggor.debug(TAG, "Dynamic Register OK!");
-            } else {
-                Loggor.error(TAG, "Dynamic Register failed!");
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        Loggor.debug(TAG, "Test Dynamic");
+        TXMqttDynreg dynreg = new TXMqttDynreg(mProductID, mProductSecret, mDevName, new SelfMqttDynregCallback());//初始化TXMqttDynreg
+        if (dynreg.doDynamicRegister()) {//调起动态注册
+            Loggor.debug(TAG, "Dynamic Register OK!");
+        } else {
+            Loggor.error(TAG, "Dynamic Register failed!");
         }
     }
 
@@ -111,56 +94,20 @@ public class DynregDevSampleTest {
         @Override
         public void onConnectCompleted(Status status, boolean reconnect, Object userContext, String msg) {
             String userContextInfo = "";
-
             String logInfo = String.format("onConnectCompleted, status[%s], reconnect[%b], userContext[%s], msg[%s]", status.name(), reconnect, userContextInfo, msg);
             Loggor.info(TAG, logInfo);
-            unlock();
+            unlock(); //成功连接，唤醒主线程
         }
-
-        private TXOTACallBack oTACallBack = new TXOTACallBack() {
-
-            @Override
-            public void onReportFirmwareVersion(int resultCode, String version, String resultMsg) {
-                Loggor.error(TAG, "onReportFirmwareVersion:" + resultCode + ", version:" + version + ", resultMsg:" + resultMsg);
-            }
-
-            @Override
-            public boolean onLastestFirmwareReady(String url, String md5, String version) {
-                Loggor.error(TAG, "onLastestFirmwareReady url=" + url + " version " + version);
-                return false; // false 自动触发下载升级文件  true 需要手动触发下载升级文件
-            }
-
-            @Override
-            public void onDownloadProgress(int percent, String version) {
-                Loggor.error(TAG, "onDownloadProgress:" + percent);
-            }
-
-            @Override
-            public void onDownloadCompleted(String outputFile, String version) {
-                Loggor.error(TAG, "onDownloadCompleted:" + outputFile + ", version:" + version);
-
-                mqttconnection.reportOTAState(TXOTAConstansts.ReportState.DONE, 0, "OK", version);
-            }
-
-            @Override
-            public void onDownloadFailure(int errCode, String version) {
-                Loggor.error(TAG, "onDownloadFailure:" + errCode);
-
-                mqttconnection.reportOTAState(TXOTAConstansts.ReportState.FAIL, errCode, "FAIL", version);
-            }
-        };
 
         @Override
         public void onConnectionLost(Throwable cause) {
             String logInfo = String.format("onConnectionLost, cause[%s]", cause.toString());
             Loggor.info(TAG, logInfo);
-
         }
 
         @Override
         public void onDisconnectCompleted(Status status, Object userContext, String msg) {
             String userContextInfo = "";
-
             String logInfo = String.format("onDisconnectCompleted, status[%s], userContext[%s], msg[%s]", status.name(), userContextInfo, msg);
             Loggor.info(TAG, logInfo);
         }
@@ -168,7 +115,6 @@ public class DynregDevSampleTest {
         @Override
         public void onPublishCompleted(Status status, IMqttToken token, Object userContext, String errMsg) {
             String userContextInfo = "";
-
             String logInfo = String.format("onPublishCompleted, status[%s], topics[%s],  userContext[%s], errMsg[%s]", status.name(), Arrays.toString(token.getTopics()), userContextInfo, errMsg);
             Loggor.debug(TAG, logInfo);
         }
@@ -176,7 +122,6 @@ public class DynregDevSampleTest {
         @Override
         public void onSubscribeCompleted(Status status, IMqttToken asyncActionToken, Object userContext, String errMsg) {
             String userContextInfo = "";
-
             String logInfo = String.format("onSubscribeCompleted, status[%s], topics[%s], userContext[%s], errMsg[%s]", status.name(), Arrays.toString(asyncActionToken.getTopics()), userContextInfo, errMsg);
             if (Status.ERROR == status) {
                 Loggor.error(TAG, logInfo);
@@ -188,7 +133,6 @@ public class DynregDevSampleTest {
         @Override
         public void onUnSubscribeCompleted(Status status, IMqttToken asyncActionToken, Object userContext, String errMsg) {
             String userContextInfo = "";
-
             String logInfo = String.format("onUnSubscribeCompleted, status[%s], topics[%s], userContext[%s], errMsg[%s]", status.name(), Arrays.toString(asyncActionToken.getTopics()), userContextInfo, errMsg);
             Loggor.debug(TAG, logInfo);
         }
@@ -326,43 +270,32 @@ public class DynregDevSampleTest {
             }
             return false;
         }
-
     }
 
     /**
      * ============================================================================== Unit Test ==============================================================================
      **/
 
-    private static Object mLock = new Object(); // 同步锁
-    private static int mCount = 0; // 加解锁条件
-    private static boolean mUnitTest = false;
+    private static final int COUNT = 1;
+    private static final int TIMEOUT = 3000;
+    private static CountDownLatch latch = new CountDownLatch(COUNT);
 
     private static void lock() {
-        synchronized (mLock) {
-            mCount = 1;  // 设置锁条件
-            while (mCount > 0) {
-                try {
-                    mLock.wait(); // 等待唤醒
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
+        latch = new CountDownLatch(COUNT);
+        try {
+            latch.await(TIMEOUT, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
     private static void unlock() {
-        if (mUnitTest) {
-            synchronized (mLock) {
-                mCount = 0;
-                mLock.notifyAll(); // 回调执行完毕，唤醒主线程
-            }
-        }
+        latch.countDown();
     }
 
     @Test
     public void testDynregDev() {
         // Loggor.saveLogs("hub/hub-device-java.log"); //保存日志到文件
-        mUnitTest = true;
         dynReg();
         lock();
         Loggor.debug(TAG, "after dynreg connect");
