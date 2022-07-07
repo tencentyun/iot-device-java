72524a0eac14e923423eea92040b06327c65c894
whalensun(孙伟) <532125505@qq.com>
2022-07-07 17:26:02 +0800

    demo修复API19 MultiDex引起的crash
    Change-Id: Iffaa08a853fc6e863955c8e4b0095c8dc992d8ff

--- explorer/device-android-demo/src/main/java/com/tencent/iot/explorer/device/android/app/App.java
+++ explorer/device-android-demo/src/main/java/com/tencent/iot/explorer/device/android/app/App.java
@@ -1,11 +1,20 @@
 package com.tencent.iot.explorer.device.android.app;
 
 import android.app.Application;
+import android.content.Context;
+
+import androidx.multidex.MultiDex;
 
 public class App extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
     }
 }
