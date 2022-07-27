60d04b08c3251d937634ee23489b505098cc4c29
archurtan <archurtan@tencent.com>
2022-07-27 15:52:46 +0800

    修复token为空的crash
    Change-Id: I636fbab052ea49bb6e302ab00370e3db46048c44

--- explorer/explorer-device-android/src/main/java/com/tencent/iot/explorer/device/android/http/IoTAuth.kt
+++ explorer/explorer-device-android/src/main/java/com/tencent/iot/explorer/device/android/http/IoTAuth.kt
@@ -1,6 +1,5 @@
 package com.tencent.iot.explorer.device.android.http
 
-import android.text.TextUtils
 
 /**
  * SDK授权认证管理
@@ -10,9 +9,6 @@ object IoTAuth {
     private var expiredTime = 0L //秒
 
     fun init(token: String, expiredTime: Long) {
-        if (TextUtils.isEmpty(token)) {
-            throw Exception("Access token can not be empty")
-        }
         this.accessToken = token
         this.expiredTime = expiredTime
     }
